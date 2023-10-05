4d9b10e17862547846029fcdfcd25bcaa5557ba6
OliverKung <43801635+OliverKung@users.noreply.github.com>
2023-10-05 23:51:24 +0800

    add some universal interface
    Try to choose configuration file type from: YAML/JSON/INI/XML/TOML
    At this moment, I think TOML or YAML will be the final choice of this project.
    INI file is good and it can be compatible with DMM-Tools that developed by ZDD. But the INI file is UGLY, I don't think I can stand it

--- InstruDriver/UIP_OSC.py
+++ InstruDriver/UIP_OSC.py
@@ -1,7 +1,7 @@
 # MSO5000 Series Oscilloscope python vxi11 lib.
 import vxi11
 import serial
-import socket
+import importlib
 from enum import Enum
 import os
 import time
@@ -20,20 +20,23 @@ class UIP_OSC:
             case(interface_Enum.serial):
                 try:
                     self.serialinterface = serial.Serial(self.addr,baudrate=baudrate,timeout=5)
+                    self.instr = self.serialinterface
                 except:
                     print("Failed to Create Serial Bus at "+self.addr)
             case(interface_Enum.socket):
                 try:
                     self.socketinterface = instru_socket.instru_socket()
                     self.socketinterface.connect((self.addr,port))
+                    self.instr = self.socketinterface
                 except:
                     print("Failed to Create Socket Bus at "+self.addr)
             case(interface_Enum.LXI):
                 try:
                     self.LXIinterface = vxi11.Instrument(self.addr)
+                    self.instr = self.LXIinterface
                 except:
                     print("Failed to Create LXI Bus at "+self.addr)
-        self.instr = 
+        ## beep of OSC to confirm connected
         self.instr.write("SYST:BEEP ON")
         print(self.instr.ask("*IDN?"))
         self.instr.write("SYST:BEEP OFF")
@@ -42,54 +45,17 @@ class UIP_OSC:
         self.instr.write(":AUT")
         time.sleep(1)
     
-    def voltage(self,channel:channel_number,items:wave_parameter):
-        self.instr=vxi11.Instrument(self.addr)
+    def voltage(self,channel,items):
         cmd = ":MEAS:ITEM? "+items.value+","+channel.value
         return float(self.instr.ask(cmd))
 
-    def freq(self,channel:channel_number):
-        self.instr=vxi11.Instrument(self.addr)
+    def freq(self,channel):
         cmd = ":MEAS:ITEM? FREQ,"+channel.value
         return float(self.instr.ask(cmd))
 
-    def phase(self,channelA:channel_number,channelB:channel_number):
-        self.instr=vxi11.Instrument(self.addr)
+    def phase(self,channelA,channelB):
         cmd = ":MEAS:ITEM? RRPH,"+channelA.value+","+channelB.value
         return float(self.instr.ask(cmd))
-
-    def saveChanneltoFile(self,\
-        file_name:str,channel:channel_number,\
-        data_mode:memory_store_method=memory_store_method.screen_only,\
-        memory_length:int=1000):
-        with open(file_name,"w") as f:
-            print("Store "+str(channel)+" "+str(memory_length)+" points data to "+file_name)
-            if(data_mode==memory_store_method.screen_only):
-                self.instr.write(":WAV:MODE NORM")
-                self.instr.write(":WAV:POIN "+str(memory_length))
-                self.instr.write(":WAV:FORMAT ASCII")
-                data_line=self.instr.ask(":WAV:DATA?")
-                data_point=data_line.split(",")
-                f.write("Voltage\r\n")
-                data_point[0]=data_point[0][11:]
-                for data in data_point:
-                    f.write(data+"\r\n")
-            if(data_mode==memory_store_method.RAW_data):
-                self.instr.write(":WAV:MODE RAW")
-                self.instr.write(":WAV:POIN "+str(memory_length))
-                self.instr.write(":WAV:FORMAT ASCII")
-                self.instr.write(":STOP")
-                print("start time:")
-                print(time.time())
-                data_line=self.instr.ask(":WAV:DATA?")
-                print(time.time())
-                print(data_line)
-                data_point=data_line.split(",")
-                f.write("Voltage\r\n")
-                data_point[0]=data_point[0][11:]# remove head meaning less bytes
-                for data in data_point:
-                    f.write(data+"\r\n")
-                self.instr.write(":RUN")
-            print(channel.value+" Data of "+self.model+" locates at "+self.addr+" saved to "+file_name)
 
     def setAcquire(self,memdepth:memory_store_depth=memory_store_depth.depth_AUTO,\
         samplemode:sample_method=sample_method.normal):
@@ -109,22 +75,22 @@ class UIP_OSC:
     def setTimebaseScale(self,timebase_scale):
         self.instr.write(":TIM:SCAL "+str(timebase_scale))
     
-    def setChannelOffet(self,channel:channel_number,offset):
+    def setChannelOffet(self,channel,offset):
         self.instr.write(":"+channel.value+":OFFS "+str(offset))
 
-    def getChannelScale(self,channel:channel_number):
+    def getChannelScale(self,channel):
         return float(self.instr.ask(":"+channel.value+":SCAL?"))
     
-    def setChannelScale(self,channel:channel_number,scale):
+    def setChannelScale(self,channel,scale):
         self.instr.write(":"+channel.value+":SCAL "+str(scale))
     
     def getTimebaseScale(self):
         return float(self.instr.ask(":TIM:SCAL?"))
     
-    def setChannelCouple(self,channel:channel_number,couple:couple_type):
+    def setChannelCouple(self,channel,couple:couple_type):
         self.instr.write(":"+channel.value+":COUP "+couple.value)
     
-    def setTriggerChannel(self,channel:channel_number):
+    def setTriggerChannel(self,channel):
         self.instr.write(":TRIG:EDGE:SOUR "+channel.value)
     
     def setTriggerLevel(self,voltage):
@@ -133,13 +99,13 @@ class UIP_OSC:
     def setAverageTimes(self,averagetimes):
         self.instr.write(":ACQ:AVER "+str(2**averagetimes))
     
-    def setChannelAtte(self,channel:channel_number,atte):
+    def setChannelAtte(self,channel,atte):
         self.instr.write(":"+channel.value+":PROB "+atte)
     
-    def setChannelUnit(self,channel:channel_number,unit:str):
+    def setChannelUnit(self,channel,unit:str):
         self.instr.write(":"+channel.value+":UNIT "+unit)
     
-    def getChannelAtte(self,channel:channel_number):
+    def getChannelAtte(self,channel):
         Atte=self.instr.ask(":"+channel.value+":PROB?")
         return float(Atte)
     
